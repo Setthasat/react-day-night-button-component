a230e274a15178ac50147618d0fc77ee5258a7a1
Setthasat <setthasat2007@gmail.com>
2023-04-28 15:54:05 +0700

    add background image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
 import { BsSun, BsFillMoonFill } from 'react-icons/bs'
 
+
 function App() {
 
   const [open, setOpen] = useState(false);
@@ -16,13 +17,19 @@ function App() {
   }, [open]);
 
   return (
-    <div className='bg-black/90  w-full h-screen flex justify-center items-center'>
+    <div className='w-full h-screen flex justify-center items-center'>
+      {open === false ? (
+        <img src='../public/day.jpeg' className='absolute h-screen w-screen duration-700 bg-black/40'/>
+      ) : (
+        <img src='../public/night.webp' className='absolute h-screen w-screen duration-700 bg-black/40'/>
+      )}
+      
       <div>
         <div className='bg-transparent backdrop-blur-3xl shadow-2xl duration-300 w-[18rem] mx-2 h-[6rem] rounded-full px-2 flex items-center'>
           {open === true ? (
-            <motion.button onClick={handleOpen} className='bg-black/60 duration-[450ms] h-[5rem] w-[5rem] rounded-full flex justify-center items-center' animate={{x:185}}><BsFillMoonFill color='white' size={40}/></motion.button>
+            <motion.button onClick={handleOpen} className='bg-black/30 duration-[450ms] h-[5rem] w-[5rem] rounded-full flex justify-center items-center' animate={{x:185}}><BsFillMoonFill color='white' size={40}/></motion.button>
           ) : (
-            <motion.button onClick={handleOpen} className='bg-white/60 duration-[450ms] h-[5rem] w-[5rem] rounded-full flex justify-center items-center' ><BsSun size={40}/></motion.button>
+            <motion.button onClick={handleOpen} className='bg-white/80 duration-[450ms] h-[5rem] w-[5rem] rounded-full flex justify-center items-center' ><BsSun size={40}/></motion.button>
           )}
         </div>
       </div>
