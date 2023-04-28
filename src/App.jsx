a2aee8ee0c56dedbea42b774cdce888ec6b6c2e7
Setthasat <setthasat2007@gmail.com>
2023-04-28 14:01:37 +0700

    add button layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import { motion } from "framer-motion";
 
 function App() {
- 
+
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event) => {
+    event.preventDefault();
+    setOpen((prev) => prev = !prev);
+  };
+
+  useEffect(() => {
+    console.log(open);
+  }, [open]);
+
   return (
-    <div>
-     
+    <div className='bg-black/90  w-full h-screen flex justify-center items-center'>
+      <div>
+        <div className='bg-white w-[18rem] h-[6rem] rounded-full flex items-center'>
+          {open === true ? (
+            <motion.button onClick={handleClose} className='bg-black h-[5rem] w-[5rem] mx-2 rounded-full' animate={{x:190}}></motion.button>
+          ) : (
+            <motion.button onClick={handleClose} className='bg-black h-[5rem] w-[5rem] mx-2 rounded-full' ></motion.button>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
